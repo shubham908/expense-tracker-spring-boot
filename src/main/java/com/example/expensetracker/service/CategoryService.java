278c6eb70ac14a5cae1fb1e3adf917c17818d331
Shubham Kumar <shubham908@gmail.com>
2021-02-13 13:42:54 +0530

    changing access specifiers

--- src/main/java/com/example/expensetracker/service/CategoryService.java
+++ src/main/java/com/example/expensetracker/service/CategoryService.java
@@ -2,6 +2,8 @@ package com.example.expensetracker.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +13,17 @@ import com.example.expensetracker.repository.CategoryRepository;
 @Service
 public class CategoryService {
 	
-	private CategoryRepository categoryRepository;
+	private final CategoryRepository categoryRepository;
+	private final Logger LOGGER = LoggerFactory.getLogger(CategoryService.class);
 	
 	@Autowired
-	public CategoryService(CategoryRepository categoryRepository) {
+	public CategoryService(final CategoryRepository categoryRepository) {
 		this.categoryRepository = categoryRepository;
 	}
 	
 	public List<Category> listCategories() {
+		LOGGER.info("Returning list of categories");
+
 		return categoryRepository.findAll();
 	}
 }
